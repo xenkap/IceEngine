0d67e7a7ee362f6dec620e51b18ecef9325037a9
xenkap <103583353+xenkap@users.noreply.github.com>
2022-05-20 16:06:57 +0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -140,7 +140,7 @@ class MainMenuState extends MusicBeatState
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24,  FlxG.width, "Mic'd Up v" + micdEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24,  FlxG.width - 48, "Mic'd Up v" + micdEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
