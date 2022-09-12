2ca8dec026a85c5de0335e847994999922756796
WindowsWhistler <55015767+bot-beep-boop@users.noreply.github.com>
2022-09-12 17:04:42 +0800

    fix my last commit :skull:

--- source/flixel/system/ui/FlxSoundTray.hx
+++ source/flixel/system/ui/FlxSoundTray.hx
@@ -136,9 +136,7 @@ class FlxSoundTray extends Sprite
 			if (sound != null)
 			{
 				var soundVar = FlxG.sound.load(sound);
-				//soundVar.pitch = FlxMath.remapToRange(FlxG.sound.volume, 0, 1, 0.2, 1);
                 soundVar.pitch = FlxMath.remapToRange(FlxG.sound.volume, 0, 1, 0.2, 1);
-                Sys.println(soundVar.pitch);
 				soundVar.play();
 			}
 		}
