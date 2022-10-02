9b9cb7622e081136788d1a7300441d0bfd2403c2
WindowsWhistler <55015767+bot-beep-boop@users.noreply.github.com>
2022-10-02 08:43:17 +0800

    Fix icon for discord and added some src of crash handler

--- CrashHandler/Source/Main.hx
+++ CrashHandler/Source/Main.hx
@@ -1,11 +1,27 @@
 package;
 
-import lime.app.Application;
+import flixel.addons.ui.FlxUIState;
+import flixel.FlxG;
+import flixel.FlxGame;
+import openfl.Lib;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.display.StageScaleMode;
+import openfl.events.UncaughtErrorEvent;
 
-class Main extends Application
+class Main extends FlxUIState
 {
+	var warnText:FlxText;
+	public function main():Void
+	{
+		Lib.current.addChild(new Main());
+	}
 	public function new()
 	{
-		super();
+		super.create();
+		print("")
 	}
 }
