82bb4df2c2a4045966187d5b56618136b72f9fc4
xenkap <103583353+xenkap@users.noreply.github.com>
2022-10-14 19:21:08 +0800

    aaaaaaaaaAAAAAAAAAAA (doesnt work)

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -26,8 +26,8 @@ class MasterEditorMenu extends MusicBeatState
 		'Dialogue Editor',
 		'Dialogue Portrait Editor',
 		'Character Editor',
-		'Chart Editor',
-		'Stage Creator'
+		'Stage Maker',
+		'Chart Editor'
 	];
 	private var grpTexts:FlxTypedGroup<Alphabet>;
 	private var directories:Array<String> = [null];
--- source/editors/StageMakerState.hx
+++ source/editors/StageMakerState.hx
@@ -45,14 +45,13 @@ using StringTools;
  */
 class StageMakerState extends MusicBeatState
 {
-	var bgLayer:FlxTypedGroup<FlxSprite>;
+	var bgLayer:FlxTypedGroup<BGSprite>;
+	var bgMap:Map<String, BGSprite>;
 	var charLayer:FlxTypedGroup<Character>;
 
-	public function new(daAnim:String = 'spooky', goToPlayState:Bool = true)
+	public function new()
 	{
 		super();
-		this.daAnim = daAnim;
-		this.goToPlayState = goToPlayState;
 	}
 
 	var UI_box:FlxUITabMenu;
@@ -63,6 +62,9 @@ class StageMakerState extends MusicBeatState
 
 	var cameraFollowPointer:FlxSprite;
 	var camFollow:FlxObject;
+
+	var imageName:FlxUIInputText;
+	var imagePath:FlxUIInputText;
 
 	override function create()
 	{
@@ -79,39 +81,8 @@ class StageMakerState extends MusicBeatState
 		FlxG.cameras.add(camMenu);
 		FlxCamera.defaultCameras = [camEditor];
 
-		bgLayer = new FlxTypedGroup<FlxSprite>();
-		add(bgLayer);
-		charLayer = new FlxTypedGroup<Character>();
-		add(charLayer);
-
-		var pointer:FlxGraphic = FlxGraphic.fromClass(GraphicCursorCross);
-		cameraFollowPointer = new FlxSprite().loadGraphic(pointer);
-		cameraFollowPointer.setGraphicSize(40, 40);
-		cameraFollowPointer.updateHitbox();
-		cameraFollowPointer.color = FlxColor.WHITE;
-		add(cameraFollowPointer);
-
-		var tipTextArray:Array<String> = "E/Q - Camera Zoom In/Out
-		\nR - Reset Camera Zoom
-		\nJKLI - Move Camera
-		\nT - Reset Current Offset
-		\nHold Shift to Move 10x faster\n".split('\n');
-
-		for (i in 0...tipTextArray.length - 1)
-		{
-			var tipText:FlxText = new FlxText(FlxG.width - 320, FlxG.height - 15 - 16 * (tipTextArray.length - i), 300, tipTextArray[i], 12);
-			tipText.cameras = [camHUD];
-			tipText.setFormat(null, 12, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE_FAST, FlxColor.BLACK);
-			tipText.scrollFactor.set();
-			tipText.borderSize = 1;
-			add(tipText);
-		}
-
-		FlxG.camera.follow(camFollow);
-
 		var tabs = [
-			// {name: 'Offsets', label: 'Offsets'},
-			{name: 'Settings', label: 'Settings'},
+			{name: 'Main Tab', label: 'Main Tab'}
 		];
 
 		UI_box = new FlxUITabMenu(null, tabs, true);
@@ -121,6 +92,57 @@ class StageMakerState extends MusicBeatState
 		UI_box.x = FlxG.width - 275;
 		UI_box.y = 25;
 		UI_box.scrollFactor.set();
+
+		var tab_group = new FlxUI(null, UI_box);
+		tab_group.name = "Main Tab";
+
+		imageName = new FlxUIInputText(15, 30, 200, 'stageFront', 8);
+		imagePath = new FlxUIInputText(imageName.x, imageName.x + 50, 200, 'stageFront', 8);
+		var addImage:FlxButton = new FlxButton(imageName.x, imagePath.y + 50, "Add Image", function()
+		{
+			trace(imagePath.text + ' ' + imageName.text);
+			var bgFunny:BGSprite = new BGSprite(imagePath.text);
+			bgLayer.add(bgFunny);
+			bgMap.set(imageName.text, bgFunny);
+		});
+
+		tab_group.add(new FlxText(imageName.x, imageName.y - 18, 0, 'Image Name:'));
+		tab_group.add(imageName);
+		tab_group.add(new FlxText(imageName.x, imagePath.y - 18, 0, 'Image Path:'));
+		tab_group.add(imagePath);
+		tab_group.add(addImage);
+		UI_box.addGroup(tab_group);
+
+		bgLayer = new FlxTypedGroup<BGSprite>();
+		add(bgLayer);
+		charLayer = new FlxTypedGroup<Character>();
+		add(charLayer);
+		add(UI_box);
+
+		// var pointer:FlxGraphic = FlxGraphic.fromClass(GraphicCursorCross);
+		// cameraFollowPointer = new FlxSprite().loadGraphic(pointer);
+		// cameraFollowPointer.setGraphicSize(40, 40);
+		// cameraFollowPointer.updateHitbox();
+		// cameraFollowPointer.color = FlxColor.WHITE;
+		// add(cameraFollowPointer);
+
+		// var tipTextArray:Array<String> = "E/Q - Camera Zoom In/Out
+		// \nR - Reset Camera Zoom
+		// \nJKLI - Move Camera
+		// \nT - Reset Current Offset
+		// \nHold Shift to Move 10x faster\n".split('\n');
+
+		// for (i in 0...tipTextArray.length - 1)
+		// {
+		// 	var tipText:FlxText = new FlxText(FlxG.width - 320, FlxG.height - 15 - 16 * (tipTextArray.length - i), 300, tipTextArray[i], 12);
+		// 	tipText.cameras = [camHUD];
+		// 	tipText.setFormat(null, 12, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE_FAST, FlxColor.BLACK);
+		// 	tipText.scrollFactor.set();
+		// 	tipText.borderSize = 1;
+		// 	add(tipText);
+		// }
+
+		FlxG.camera.follow(camFollow);
 
 		FlxG.mouse.visible = true;
 
